3e03019082aaa8360d0def814abcff2fc62e8b0b
EliotFerragni <eliot.ferragni@epfl.ch>
2020-06-03 09:53:13 +0200

    -Added main.c

---
+++ src/main.c
@@ -0,0 +1,34 @@
+/**
+ * @file	main.c
+ * @brief  	Main file of the e-puck3 programmer firmware
+ * 
+ * @written by  	Eliot Ferragni
+ * @creation date   03.06.2020
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "ch.h"
+#include "hal.h"
+
+
+
+int main(void) {
+	
+	/*
+	* System initializations.
+	* - HAL initialization, this also initializes the configured device drivers
+	*   and performs the board-specific initializations.
+	* - Kernel initialization, the main() function becomes a thread and the
+	*   RTOS is active.
+	*/
+	halInit();
+	chSysInit();
+
+  
+	while (true){
+		chThdSleepMilliseconds(500);
+    }
+
+}
