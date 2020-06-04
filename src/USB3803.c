ba460fa696bc454c81ada440069b6a1a6aacd9ea
EliotFerragni <eliot.ferragni@epfl.ch>
2020-06-04 12:05:43 +0200

    -Added files for the USB hub USB3803

---
+++ src/USB3803.c
@@ -0,0 +1,9 @@
+/**
+ * @file    USB3803.c
+ * @brief   Functions to configure the USB3803 USB2 Hub over I2C.
+ *
+ * @author  Eliot Ferragni
+ */
+
+
+
---
+++ src/USB3803.h
@@ -0,0 +1,15 @@
+#ifndef USB3803_H
+#define USB3803_H
+
+/**
+ * @file    USB3803.c
+ * @brief   Functions to configure the USB3803 USB2 Hub over I2C.
+ *
+ * @author  Eliot Ferragni
+ */
+
+
+//////////////////// PROTOTYPES PUBLIC FUNCTIONS /////////////////////
+
+
+#endif /* USB3803_H*/
