ce75476ad0e9ac03ad5a06bcc0ae2a21cdee0427
EliotFerragni <eliot.ferragni@epfl.ch>
2020-07-01 10:35:40 +0200

    -Added tests functions for the power part

--- src/user_shell.c
+++ src/user_shell.c
@@ -24,7 +24,45 @@ static THD_WORKING_AREA(waShell,2048);
 static char sc_histbuf[SHELL_MAX_HIST_BUFF];
 static char* completion_buffer[SHELL_MAX_COMPLETIONS];
 
+static void cmd_set_vbus_priority(BaseSequentialStream *chp, int argc, char *argv[]){
+	if(argc == 1){
+		char *endptr;
+    uint8_t enabled = strtol(argv[0], &endptr, 0);
+
+    if(enabled){
+    	palSetLine(LINE_PRIORITY_TO_VBUS);
+    	chprintf(chp, "vbus_priority enabled\r\n");
+    }else{
+    	palClearLine(LINE_PRIORITY_TO_VBUS);
+    	chprintf(chp, "vbus_priority disabled\r\n");
+    }
+	}else{
+		chprintf(chp, "USAGE: set_vbus_priority 1|0\r\n");
+		chprintf(chp, "actual state: %d\r\n", palReadLine(LINE_PRIORITY_TO_VBUS));
+	}
+}
+
+static void cmd_set_inrush_limit(BaseSequentialStream *chp, int argc, char *argv[]){
+	if(argc == 1){
+		char *endptr;
+    uint8_t enabled = strtol(argv[0], &endptr, 0);
+
+    if(enabled){
+    	palClearLine(LINE_BAT_PLUS_INRUSH_LIMIT_n);
+    	chprintf(chp, "inrush_limit enabled\r\n");
+    }else{
+    	palSetLine(LINE_BAT_PLUS_INRUSH_LIMIT_n);
+    	chprintf(chp, "inrush_limit disabled\r\n");
+    }
+	}else{
+		chprintf(chp, "USAGE: set_inrush_limit 1|0\r\n");
+		chprintf(chp, "actual state: %d\r\n", !palReadLine(LINE_BAT_PLUS_INRUSH_LIMIT_n));
+	}
+}
+
 static const ShellCommand commands[] = {
+	{"set_vbus_priority", cmd_set_vbus_priority},
+	{"set_inrush_limit", cmd_set_inrush_limit},
   USB_PD_CONTROLLER_SHELL_CMD
   THREADS_UTILITIES_SHELL_CMD
   {NULL, NULL}
