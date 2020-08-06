3189a6e1fc9bc7392deae30536dccd3997a97f5a
EliotFerragni <eliot.ferragni@epfl.ch>
2020-08-06 12:23:53 +0200

    -Added serial datagram and uart_cmd files

---
+++ src/uart_cmd.c
@@ -0,0 +1,7 @@
+/**
+ * @file	uart_cmd.c
+ * @brief  	Manages the uart_cmd transmission between the f746 and the f779
+ * 
+ * @written by  	Eliot Ferragni
+ * @creation date	06.08.2020
+ */
---
+++ src/uart_cmd.h
@@ -0,0 +1,13 @@
+/**
+ * @file	uart_cmd.h
+ * @brief  	Manages the uart_cmd transmission between the f746 and the f779
+ * 
+ * @written by  	Eliot Ferragni
+ * @creation date	06.08.2020
+ */
+
+#ifndef UART_CMD_H
+#define UART_CMD_H
+
+
+#endif  /* UART_CMD_H */
